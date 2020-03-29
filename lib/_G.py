68c5c4026ddfa0b3164b3680bae75d703165505b
ken1882 <qj6w94g0q@gmail.com>
2020-03-29 18:58:49 +0800

    update basic loop

--- lib/_G.py
+++ lib/_G.py
@@ -1,1 +1,19 @@
 AppHwnd = 0
+
+BaseUpdateFactor = 120
+UpdateDuration   = (1 / BaseUpdateFactor)
+ScriptUpdateTime = BaseUpdateFactor
+CurScriptTick    = BaseUpdateFactor
+
+def change_update_speed(n):
+  global BaseUpdateFactor, UpdateDuration
+  global ScriptUpdateTime, CurScriptTick
+  BaseUpdateFactor = n
+  UpdateDuration   = (1 / BaseUpdateFactor)
+  ScriptUpdateTime = BaseUpdateFactor
+  CurScriptTick    = BaseUpdateFactor
+
+Flags = {
+  'running': True,
+  'paused': False
+}
--- main.py
+++ main.py
@@ -1,8 +1,46 @@
 import sys
 sys.path.append('./lib')
+import _G, const, util, Input
 
-import _G, const
-import util, input
+def start():
+  util.init()
+  
+  if _G.AppHwnd == 0:
+    print("App not found, aborting")
+    return exit()
 
-util.init()
-util.activate_window(_G.AppHwnd)
+  util.activate_window(_G.AppHwnd)
+  while _G.Flags['running']:
+    main_loop()
+
+def main_loop():
+  util.uwait(_G.UpdateDuration, False)
+  
+  update_basic()
+  _G.CurScriptTick += 1
+  
+  if _G.CurScriptTick >= _G.ScriptUpdateTime:
+    _G.CurScriptTick = 0
+    if not _G.Flags['paused']:
+      update_script()
+      Input.clear_cache()
+
+def update_basic():
+  update_input()
+
+def update_input():
+  Input.update()
+  # pause program
+  if Input.is_trigger(Input.keymap.kF8, True):
+    _G.Flags['paused'] ^= True
+    print(f"Paused: {_G.Flags['paused']}")
+  
+  # terminate program
+  elif Input.is_trigger(Input.keymap.kF9, True):
+    _G.Flags['running'] = False
+
+def update_script():
+  util.get_app_rect()
+
+
+start()
